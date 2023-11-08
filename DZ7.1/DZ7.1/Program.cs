28509816b95bcc78d9e11533f2d08c8a044c410b
kiriquellK <kiryaep@gmail.com>
2023-11-09 01:12:25 +0300

    fix 7.1

--- DZ7.1/DZ7.1/Program.cs
+++ DZ7.1/DZ7.1/Program.cs
@@ -4,7 +4,47 @@
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите характеристики сферы:");
+            Console.WriteLine("Радиус:");
+            bool isInputRadius = int.TryParse(Console.ReadLine(), out int inputRadius);
+            if (!isInputRadius || inputRadius < 0)
+            {
+                Console.WriteLine("Некорректный радиус");
+            }
+            else
+            {
+                Console.WriteLine("Координата X");
+                bool isinputXCenter = double.TryParse(Console.ReadLine(), out double inputXCenter);
+                Console.WriteLine("Координата Y");
+                bool isinputYCenter = double.TryParse(Console.ReadLine(), out double inputYCenter);
+                Console.WriteLine("Координата Z");
+                bool isinputZCenter = double.TryParse(Console.ReadLine(), out double inputZCenter);
+                if (!isinputXCenter || !isinputYCenter || !isinputZCenter)
+                {
+                    Console.WriteLine("Некорректные координаты");
+                }
+                else
+                {
+                    Sphere sphere = new Sphere();
+                    Console.WriteLine($"Построена сфера с центром в точке {sphere.GetCenter()} и радиусом {sphere.Radius}");
+                    Sphere sphere1 = new Sphere(inputRadius, inputXCenter, inputYCenter, inputZCenter);
+                    sphere1.Radius = inputRadius;
+                    sphere1.SetCenter(inputXCenter, inputYCenter, inputZCenter);
+                    Console.WriteLine($"Построена сфера с центром в точке {sphere1.GetCenter()} и радиусом {sphere1.Radius}");
+                    Console.WriteLine($"Объем заданной сферы: {sphere1.GetVolume()}");
+                    Console.WriteLine($"Площадь поверхности сферы: {sphere1.GetSquare()}");
+                    Console.WriteLine($"Принадлежит ли точка с координатами (4,8,1): {sphere1.isPointInside(4, 8, 1)}");
+                    Console.WriteLine($"Принадлежит ли точка с координатами (-1,2,1): {sphere1.isPointInside(-1, 2, 1)}");
+                    Console.WriteLine("Вторая сфера:");
+                    Sphere sphere2 = new Sphere(6, 1, 0.5, 4);
+                    Console.WriteLine($"Центр сферы:{sphere2.GetCenter()}");
+                    Console.WriteLine($"Радиус сферы:{sphere2.Radius}");
+                    Console.WriteLine("Третья сфера");
+                    Sphere sphere3 = new Sphere(5.5);
+                    Console.WriteLine($"Центр сферы: {sphere3.GetCenter()}");
+                    Console.WriteLine($"Радиус сферы: {sphere3.Radius}");
+                }
+            }
         }
     }
 }
--- DZ7.1/DZ7.1/Sphere.cs
+++ DZ7.1/DZ7.1/Sphere.cs
@@ -8,16 +8,16 @@
         internal double _zCenter;
         internal double Radius
         {
-            get 
+            get
             {
                 return _radius;
             }
-            set 
+            set
             {
                 _radius = value;
             }
         }
-        internal Sphere() 
+        internal Sphere()
         {
             _radius = 1;
             _xCenter = 0;
@@ -38,6 +38,28 @@
             _yCenter = yCenter;
             _zCenter = zCenter;
         }
-        internal 
+        internal double GetVolume()
+        {
+            return 4 / 3 * Math.PI * _radius * _radius * _radius;
+        }
+        internal double GetSquare()
+        {
+            return 4 * Math.PI * _radius * _radius;
+        }
+        internal string GetCenter()
+        {
+            return _xCenter.ToString() + "" + _yCenter.ToString() + "" + _yCenter;
+        }
+        internal void SetCenter(double xCenter, double yCenter, double zCenter)
+        {
+            _xCenter = xCenter;
+            _yCenter = yCenter;
+            _zCenter = zCenter;
+        }
+        internal bool isPointInside(double x, double y, double z)
+        {
+            double squaredDistance = (x - _xCenter) * (x - _xCenter) + (y - _yCenter) * (y - _yCenter) + (z - _zCenter) * (z - _zCenter);
+            return squaredDistance < _radius * _radius;
+        }
     }
 }
